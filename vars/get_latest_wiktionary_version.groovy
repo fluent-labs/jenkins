b2b4e4d341feda65217de5a384d6f3c1d5370999
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-23 01:44:57 +0200

    Make things into a library

---
+++ vars/get_latest_wiktionary_version.groovy
@@ -0,0 +1,12 @@
+def call(String: dictionary) {
+  def rss_url = "https://dumps.wikimedia.org/$dictionary/latest/$dictionary-latest-pages-meta-current.xml.bz2-rss.xml"
+  def filename = "$dictionary-latest-pages-meta-current.xml.bz2-rss.xml"
+
+  sh "wget $rss_url"
+  def xml = sh script: "cat $filename", returnStdout: true
+
+  def parsed = new XmlSlurper().parseText(xml)
+  def link = parsed.channel.link.text()
+  
+  return (link =~ /.*\/([0-9]+)/)[ 0 ][ 1 ].toString().trim()
+}
