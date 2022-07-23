629b3f7e9e1f56604d53980635e2232dfb63685e
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-23 16:16:25 +0200

    Make the containers just work

--- deploy/spark.groovy
+++ deploy/spark.groovy
@@ -5,13 +5,27 @@ pipeline {
         apiVersion: v1
         kind: Pod
         metadata:
-          namespace: content
+          namespace: jobs
         spec:
           imagePullSecrets:
           - 'dockerhub'
+          volumes:
+          - name: sbt-cache
+            persistentVolumeClaim:
+              claimName: sbt-cache
           containers:
-          - name: spark
-            image: lkjaero/jenkins-runners:spark
+          - name: sbt
+            image: lkjaero/jenkins-runners:sbt
+            command:
+            - sleep
+            args:
+            - 99d
+            volumeMounts:
+            - name: sbt-cache
+              mountPath: /root/.cache/coursier/v1/
+          - name: s3
+            image: lkjaero/jenkins-runners:s3
+            imagePullPolicy: Always
             command:
             - sleep
             args:
@@ -27,13 +41,6 @@ pipeline {
                 secretKeyRef:
                   name: do-spaces-access-key
                   key: password
-            volumes:
-            - name: sbt-cache
-              persistentVolumeClaim:
-                claimName: sbt-cache
-            volumeMounts:
-            - name: sbt-cache
-              mountPath: /root/.cache/coursier/v1/
       '''
     }
   }
@@ -42,15 +49,15 @@ pipeline {
   stages {
     stage("Build the jar") {
       steps {
-        container('spark') {
-          git branch: 'develop', url: 'https://github.com/fluent-labs/jobs.git'
+        container('sbt') {
+          git branch: 'master', url: 'https://github.com/fluent-labs/jobs.git'
           sh 'sbt assembly'
         }
       }
     }
     stage("Deploy the jar") {
       steps {
-        container('spark') {
+        container('s3') {
           sh "s3cmd --host 'fra1.digitaloceanspaces.com' --host-bucket '%(bucket)s.fra1.digitaloceanspaces.com' put target/scala-2.13/jobs.jar s3://definitions/jobs.jar"
         }
       }
